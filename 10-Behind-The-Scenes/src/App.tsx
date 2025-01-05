96b29e4051847478e0f0dfbf6680e6c25ef428b3
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-01-05 23:56:51 +0700

    small fix

--- 10-Behind-The-Scenes/src/App.tsx
+++ 10-Behind-The-Scenes/src/App.tsx
@@ -1,33 +1,20 @@
 import Header from "./components/Header";
 import Counter from "./components/Counter/Counter";
 import { log } from "./log";
+import CounterConfigure from "./components/CounterConfigure";
 
 import { useState } from "react";
 
 function App() {
   log("<App /> rendered");
 
-  const [enteredNumber, setEnteredNumber] = useState<number>(0);
   const [chosenCount, setChosenCount] = useState<number>(0);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEnteredNumber(+event.target.value);
-  };
-
-  const handleSetClick = () => {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
-  };
 
   return (
     <>
       <Header />
       <main>
-        <section id="configure-counter">
-          <h2>Set Counter</h2>
-          <input type="number" onChange={handleChange} value={enteredNumber} />
-          <button onClick={handleSetClick}>Set</button>
-        </section>
+        <CounterConfigure handleOnClick={setChosenCount} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
---
+++ 10-Behind-The-Scenes/src/components/CounterConfigure.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { log } from "../log";
+
+type Props = {
+  handleOnClick: (chosenCount: number) => void;
+};
+
+const CounterConfigure: React.FC<Props> = ({ handleOnClick }) => {
+  log("<CounterConfigure /> rendered", 1);
+  const [enteredNumber, setEnteredNumber] = useState<number>(0);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredNumber(Number(event.target.value));
+  };
+
+  const handleSetClick = () => {
+    handleOnClick(enteredNumber);
+    setEnteredNumber(0);
+  };
+
+  return (
+    <section id="configure-counter">
+      <h2>Set Counter</h2>
+      <input type="number" onChange={handleChange} value={enteredNumber} />
+      <button onClick={handleSetClick}>Set</button>
+    </section>
+  );
+};
+
+export default CounterConfigure;
