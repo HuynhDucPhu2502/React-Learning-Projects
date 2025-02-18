0f230259b2377eb3f14003234c6ab5fcb02e6340
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-02-18 20:24:49 +0700

    push 1-practice project

---
+++ 1-PRACTICE-Component/src/App.tsx
@@ -0,0 +1,14 @@
+import Calculator from "./components/Calculator";
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <>
+       <Calculator></Calculator>
+      <TodoList></TodoList>
+    </>
+  );
+}
+
+export default App;
