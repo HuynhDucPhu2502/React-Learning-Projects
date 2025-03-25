adaf90a592ad3138a23b3b3afd4629e5329b1451
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-03-25 18:58:13 +0700

    fix

---
+++ School/Midterm-7/src/App.jsx
@@ -0,0 +1,11 @@
+import { Calculator } from "./components/Calculator";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-gray-50 min-h-screen">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
